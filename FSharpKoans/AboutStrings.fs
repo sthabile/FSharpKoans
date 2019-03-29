772f3963837e54d35119027adc3f1e4477d95c86
stha <g17l3794@campus.ru.ac.za>
2019-03-29 19:32:53 +0200

    updated AboutString

--- FSharpKoans/AboutStrings.fs
+++ FSharpKoans/AboutStrings.fs
@@ -48,22 +48,22 @@ module ``13: String manipulation`` =
 
     [<Test>]
     let ``07 String formatting: %s format specifier`` () =
-        let result = sprintf __ "perfect"
+        let result = sprintf  "Practice makes %s" "perfect"
         result |> should equal "Practice makes perfect."
 
     [<Test>]
     let ``08 String formatting: %d format specifier`` () =
-        let result = sprintf __ 9
+        let result = sprintf "%d planets, Sir, endlessly circle, Sir" 9
         result |> should equal "9 planets, Sir, endlessly circle, Sir"
 
     [<Test>]
     let ``09 String formatting: %b format specifier`` () =
-        let result = sprintf __ true
+        let result = sprintf "It's %b, it is." true
         result |> should equal "It's true, it is."
 
     [<Test>]
     let ``10 String formatting: %c format specifier`` () =
-        let result = sprintf __ 'X'
+        let result = sprintf "%c marks the spot." 'X'
         result |> should equal "X marks the spot."
 
     // specify a precision using %.Nf, where N is an integer
@@ -71,29 +71,29 @@ module ``13: String manipulation`` =
     // The default precision is about 6, as near as I can tell.
     [<Test>]
     let ``11 String formatting: %f format specifier`` () =
-        let result = sprintf __ 2.26
-        let condensed = sprintf __ 2.26
-        let rounded = sprintf __ 2.26
+        let result = sprintf "Multiply by %.6f, then triple" 2.26
+        let condensed = sprintf "Multiply by %.2f, then triple" 2.26
+        let rounded = sprintf "Multiply by %.1f, then triple" 2.26
         result |> should equal "Multiply by 2.260000, then triple"
         condensed |> should equal "Multiply by 2.26, then triple"
         rounded |> should equal "Multiply by 2.3, then triple"
 
     [<Test>]
     let ``12 String formatting: %A format specifier`` () =
-        let result = sprintf __ [7.4; 7.31; 6.55]
+        let result = sprintf "Control scores: %A (after transform)" [7.4; 7.31; 6.55]
         result |> should equal "Control scores: [7.4; 7.31; 6.55] (after transform)"
-        let moreResult = sprintf __ (8,3,"UTC")
+        let moreResult = sprintf "The %A time-coordinate was used." (8,3,"UTC")
         moreResult |> should equal "The (8, 3, \"UTC\") time-coordinate was used."
 
    // double-up a % to get a % in.
     [<Test>]
     let ``13 String formatting: Putting a '%' sign in`` () =
-        let result = sprintf __ 94.43
+        let result = sprintf "I scored %.2f%% on the test" 94.43
         result |> should equal "I scored 94.43% on the test"
 
     [<Test>]
     let ``14 String formatting: Multiple format specifiers`` () =
-        let result = sprintf __ 3 5 0.6 "in other words" 60
+        let result = sprintf "% out of %i is %.1f, or (%s) %i percent." 3 5 0.6 "in other words" 60
         result |> should equal "3 out of 5 is 0.6, or (in other words) 60 percent."
 
    // But that's not all! See the full set of formatting capabilities here:
